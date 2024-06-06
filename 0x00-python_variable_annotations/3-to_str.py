54cdd853f0e65276cca8f162c77da9a287a7e811
arsany199 <arsanykamal30@gmail.com>
2024-06-06 07:11:53 -0700

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""model that take number float and return string"""
+
+
+def to_str(n: float) -> str:
+    """function take float number and return it in string"""
+    return (str(n))
