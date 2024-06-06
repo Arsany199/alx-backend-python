0b0756437cf443d6a3392c66e71994c8e5e897ed
arsany199 <arsanykamal30@gmail.com>
2024-06-06 06:21:09 -0700

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""model to add 2 float numbers and return float number"""
+
+
+def add(a: float, b: float) -> float:
+    """function that add a & b and return float"""
+    return (a + b)
