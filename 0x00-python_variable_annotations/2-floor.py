7b5d0bf87aee345027e48bdce1283ac090efb378
arsany199 <arsanykamal30@gmail.com>
2024-06-06 06:38:22 -0700

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""model that take a float and return the floor of the float"""
+
+
+def floor(n: float) -> int:
+    """function that take float number and return the floor in int"""
+    return (int(n))
