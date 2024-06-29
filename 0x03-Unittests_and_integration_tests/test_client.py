4a9e6023dc08ed90b74aa4d9ca4933af3ac231b6
arsany199 <arsanykamal30@gmail.com>
2024-06-29 12:37:24 -0700

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,3 +16,13 @@ class TestGithubOrgClient(unittest.TestCase):
         testmyclass = GithubOrgClient(org_name)
         testmyclass.org()
         mock.called_with_once(testmyclass.ORG_URL.format(org=org_name))
+
+    def test_public_repos_url(self):
+        """function to to unit-test GithubOrgClient._public_repos_url"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mymock:
+            mypayload = {"repos_url": "something"}
+            mymock.return_value = mypayload
+            test_class = GithubOrgClient('test')
+            res = test_class._public_repos_url
+            self.assertEqual(res, payload["repos_url"])
